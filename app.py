193c0aaac6af89293dca05b84f22c9b10bd326ba
sou1205-cc <156163348+sou1205-cc@users.noreply.github.com>
2026-05-12 14:02:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,37 @@
 from flask import Flask
-from openai import OpenAI
-import os
 
 app = Flask(__name__)
-
-client = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY")
-)
 
 @app.route("/")
 def home():
 
-    response = client.chat.completions.create(
-        model="gpt-4.1-mini",
-        messages=[
-            {
-                "role": "user",
-                "content": """
-                この応募者を簡潔に要約してください。
+    resume_text = """
+    測量経験3年。
+    GIS使用経験あり。
+    CAD操作可能。
+    """
 
-                測量経験3年。
-                GIS使用経験あり。
-                CAD操作可能。
-                """
-            }
-        ]
-    )
+    keywords = [
+        "測量",
+        "GIS",
+        "CAD",
+        "Python",
+        "測量士",
+        "測量士補"
+    ]
 
-    return response.choices[0].message.content
+    found = []
+
+    for keyword in keywords:
+        if keyword in resume_text:
+            found.append(keyword)
+
+    result = "検出スキル:<br>"
+
+    for item in found:
+        result += f"・{item}<br>"
+
+    return result
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
