963becc29c127ad0a0289ff053173c30d61dc473
sou1205-cc <156163348+sou1205-cc@users.noreply.github.com>
2026-05-12 13:51:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
 from flask import Flask
+from openai import OpenAI
+import os
 
 app = Flask(__name__)
 
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
 @app.route("/")
 def home():
-    return "AI Resume Server Running!"
+
+    response = client.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": """
+                この応募者を簡潔に要約してください。
+
+                測量経験3年。
+                GIS使用経験あり。
+                CAD操作可能。
+                """
+            }
+        ]
+    )
+
+    return response.choices[0].message.content
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
